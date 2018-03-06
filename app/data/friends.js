9d1d33bcc714006458c650af2b3fd3293fa4dae9
swcooper2014 <swcooper2014@gmail.com>
2018-03-06 15:18:50 -0500

    message

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Ariel',
+		photo: 'images/ariel.jpeg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'4',
+			'5',
+			'3',
+			'5',
+			'2',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Elsa',
+		photo: 'images/elsa.jpeg',
+		scores: [
+			'5',
+			'1',
+			'1',
+			'5',
+			'1',
+			'5',
+			'2',
+			'5',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Merida',
+		photo: 'images/merida.png',
+		scores: [
+			'5',
+			'1',
+			'1',
+			'4',
+			'4',
+			'2',
+			'5',
+			'2',
+			'2',
+			'5'
+		]
+	},
+	{
+		name: 'Jessie',
+		photo: 'images/Jessie.png',
+		scores: [
+			'3',
+			'1',
+			'2',
+			'4',
+			'5',
+			'1',
+			'4',
+			'3',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Minnie',
+		photo: 'images/minnie.jpg',
+		scores: [
+			'3',
+			'1',
+			'5',
+			'2',
+			'1',
+			'5',
+			'3',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Mickey',
+		photo: 'images/mickey.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'1',
+			'3',
+			'3',
+			'3',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Gaston',
+		photo: 'images/gaston.png',
+		scores: [
+			'5',
+			'2',
+			'1',
+			'5',
+			'4',
+			'1',
+			'5',
+			'1',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Hercules',
+		photo: 'images/hercules.jpg',
+		scores: [
+			'4',
+			'5',
+			'5',
+			'1',
+			'5',
+			'1',
+			'3',
+			'3',
+			'5',
+			'2'
+		]
+	},
+	{
+		name: 'Beast',
+		photo: 'images/beast.jpg',
+		scores: [
+			'5',
+			'1',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Simba',
+		photo: 'images/simba.png',
+		scores: [
+			'4',
+			'3',
+			'1',
+			'5',
+			'1',
+			'4',
+			'5',
+			'4',
+			'3',
+			'3'
+		]
+	}
+];
+
+module.exports = friendsArray;
