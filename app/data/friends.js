2bf378610727fa2d4f6c336754630b7ef64327be
swcooper2014 <swcooper2014@gmail.com>
2018-03-06 18:33:29 -0500

    message

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
 	{
 		name: 'Ariel',
-		photo: 'images/ariel.jpeg',
+		photo: 'https://lumiere-a.akamaihd.net/v1/images/gallery_princess_ariel_5_28dcc9e3.jpeg?height=473&mode=crop&region=0%2C0%2C2181%2C1223&width=630',
 		scores: [
 			'1',
 			'1',
@@ -17,7 +17,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Elsa',
-		photo: 'images/elsa.jpeg',
+		photo: 'https://lumiere-a.akamaihd.net/v1/images/6d7454cea6644379adc7e529c5790a28078a2823.jpeg?region=0,0,450,450&width=320',
 		scores: [
 			'5',
 			'1',
@@ -33,7 +33,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Merida',
-		photo: 'images/merida.png',
+		photo: 'https://lumiere-a.akamaihd.net/v1/images/gallery_princess_merida_8_72603f1f.jpeg?height=473&mode=crop&region=0%2C0%2C2181%2C1223&width=630',
 		scores: [
 			'5',
 			'1',
@@ -49,7 +49,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Jessie',
-		photo: 'images/Jessie.png',
+		photo: 'https://vignette.wikia.nocookie.net/disney/images/7/7a/Jessie.png/revision/latest?cb=20131106225109',
 		scores: [
 			'3',
 			'1',
@@ -65,7 +65,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Minnie',
-		photo: 'images/minnie.jpg',
+		photo: 'https://www.pinterest.com/pin/471541023471222451/',
 		scores: [
 			'3',
 			'1',
@@ -81,7 +81,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Mickey',
-		photo: 'images/mickey.jpg',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/d/d4/Mickey_Mouse.png',
 		scores: [
 			'5',
 			'5',
@@ -97,7 +97,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Gaston',
-		photo: 'images/gaston.png',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/Gaston_smirking.jpg/330px-Gaston_smirking.jpg',
 		scores: [
 			'5',
 			'2',
@@ -113,7 +113,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Hercules',
-		photo: 'images/hercules.jpg',
+		photo: 'https://vignette.wikia.nocookie.net/disney/images/a/a0/Hercules_%28character%29.jpg/revision/latest/scale-to-width-down/126?cb=20120718073554',
 		scores: [
 			'4',
 			'5',
@@ -129,7 +129,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Beast',
-		photo: 'images/beast.jpg',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/e/ee/Beast_%28Disney%29.jpg',
 		scores: [
 			'5',
 			'1',
@@ -145,7 +145,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Simba',
-		photo: 'images/simba.png',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cf/YoungSimbaSmilestlk.png/330px-YoungSimbaSmilestlk.png',
 		scores: [
 			'4',
 			'3',
